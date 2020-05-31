08e5ac3f4dfe396b6e7a47edff226782fd98835c
zzy <347420151@qq.com>
2020-05-31 15:33:23 +0800

    初始化

---
+++ src/components/home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+class home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+        console.log(this.props)
+    }
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default home;
